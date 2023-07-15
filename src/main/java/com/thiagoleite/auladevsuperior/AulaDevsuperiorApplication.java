4288c6c8f45fc6b819854599404922896619df1c
Thiago Leite <thiago.0686@gmail.com>
2023-07-15 00:00:27 -0300

    create Seed

---
+++ src/main/java/com/thiagoleite/auladevsuperior/AulaDevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoleite.auladevsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaDevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaDevsuperiorApplication.class, args);
+	}
+
+}
